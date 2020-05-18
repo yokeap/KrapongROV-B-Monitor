287d398ac534e1038b55a5aab265fdb1a339e627
Siwakorn Sukprasertchai <siwakorn.ictrl@gmail.com>
2020-05-18 15:46:48 +0700

    update with rdr2

--- ArduinoMicro/ArduinoMicro.ino
+++ ArduinoMicro/ArduinoMicro.ino
@@ -2,51 +2,42 @@
 
 const int currentPin = A0;
 const int tempPin = A1;
+const int voltagePin = A2;
 const float analogRes = 0.00488758553;
 
 int tempData[10];
 int currentData[10];
+int voltageData[10];
 
 
-float flTemp, flCurrent;
+float flTemp, flCurrent, flVoltage;
 
-StaticJsonBuffer<100> jsonBuffer;
-
-JsonObject& root = jsonBuffer.createObject();
+DynamicJsonDocument doc(1024);
 
 void setup() {
   // put your setup code here, to run once:
-//  Serial.begin(115200);
+  Serial.begin(115200);
   Serial1.begin(115200);
   pinMode(4, OUTPUT);
 }
 
 void loop() {
-  int avgCurrent = 0, avgTemp = 0;
+  uint16_t avgCurrent = 0, avgTemp = 0,avgVoltage = 0;
   // put your main code here, to run repeatedly:
   for(int i=0; i<10; i++){
     avgCurrent += analogRead(currentPin);
     avgTemp += analogRead(tempPin);
+    avgVoltage += analogRead(voltagePin);
     delay(50);
   }
   digitalWrite(4, HIGH);
-  flCurrent = ((avgCurrent / 10) * analogRes);
-  flTemp = ((avgTemp / 10) * analogRes) / 0.01;
-  root["amp"] = flCurrent;
-  root["temperature"] = flTemp;
-  root.printTo(Serial1);
-  delay(500);
+  flCurrent = (((float)avgCurrent / 10) * analogRes);
+  flTemp = (((float)avgTemp / 10) * analogRes) / 0.01;
+  flVoltage = (((float)avgVoltage / 10) * analogRes);
+  doc["amp"] = flCurrent;
+  doc["temperature"] = flTemp;
+  doc["voltage"] = flVoltage;
+  serializeJson(doc, Serial);
+  serializeJson(doc, Serial1);
   digitalWrite(4, LOW);
-//  Serial.print(flCurrent);
-//  Serial.print(",  ");
-//  Serial.print(flTemp);
-//  Serial.println(",");
-//  Serial1.print(flCurrent);
-//  Serial1.print(",  ");
-//  Serial1.print(flTemp);
-//  Serial1.println(",");
-
 }
-
-
-
